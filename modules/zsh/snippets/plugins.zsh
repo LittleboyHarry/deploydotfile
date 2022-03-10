4ee8b0fe0b439151295ca9a426d1139db1740988
LittleboyHarry <littleboyharry@qq.com>
2022-03-10 11:21:12 +0800

    impove ohmyzsh load

--- modules/zsh/snippets/plugins.zsh
+++ modules/zsh/snippets/plugins.zsh
@@ -1,14 +1,20 @@
-_load_plugins() {
+__load_my_plugins() {
+	local omz_plugins=(
+		sudo history dirhistory zsh-interactive-cd aliases alias-finder z
+		git rsync dnf extract systemd
+		command-not-found gitignore shell-proxy
+	)
 	local zplugins_dir="$HOME/.create-my-dotfiles/zsh-plugins"
+	local autosuggestions="$zplugins_dir/zsh-autosuggestions"
+	local syntax_highlighting="$zplugins_dir/zsh-syntax-highlighting"
+	omz="$zplugins_dir/ohmyzsh"
 
 	[ ! -d "$zplugins_dir" ] && return
 
-	local autosuggestions="$zplugins_dir/zsh-autosuggestions"
 	if [ -d "$autosuggestions" ]; then
 		source "$autosuggestions/zsh-autosuggestions.zsh"
 	fi
 
-	local syntax_highlighting="$zplugins_dir/zsh-syntax-highlighting"
 	if [ -d "$syntax_highlighting" ]; then
 		source "$syntax_highlighting/zsh-syntax-highlighting.zsh"
 	fi
@@ -22,25 +28,23 @@ _load_plugins() {
 		}
 	fi
 
-	local omz="$zplugins_dir/ohmyzsh"
 	if [ -d "$omz" ]; then
-		_load_omz_plugin() {
-			source "$omz/plugins/$1/$1.plugin.zsh"
+	  __load_omzplug__dir="$omz"
+		loadomzplug() {
+			source "$__load_omzplug__dir/plugins/$1/$1.plugin.zsh"
 		}
 
+		source "$omz/lib/functions.zsh"
 		source "$omz/lib/completion.zsh"
 		source "$omz/lib/key-bindings.zsh"
 		source "$omz/lib/clipboard.zsh"
-		_load_omz_plugin {copybuffer,copyfile,copydir}
+		loadomzplug {copybuffer,copyfile,copydir}
 
-		for name in \
-			sudo history dirhistory command-not-found zsh-interactive-cd aliases alias-finder\
-			extract systemd gitignore dnf shell-proxy colored-man-pages
-		do _load_omz_plugin $name;	done
+		for plugin in $omz_plugins; do loadomzplug $plugin; done
 
 		# others: aliases common-aliases git rsync vagrant
 		# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/
 	fi
 }
 
-_load_plugins
+__load_my_plugins
