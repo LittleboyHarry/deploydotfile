a581cf5074de8d50eb7a7f8f33ae905c7dca18b1
LittleboyHarry <littleboyharry@qq.com>
2022-05-13 16:13:36 +0800

    refactor: ohmyzsh plugins loader

---
+++ modules/zsh/snippets/ohmyzsh.zsh
@@ -0,0 +1,52 @@
+__dmd_load_omz() {
+    local omzdir="$HOME/.deploy-my-dotfiles/zsh-plugins/ohmyzsh"
+    [[ ! -d "$omzdir" ]] && return
+
+    local pluglst=()
+
+    lsb_release -si | grep -qi "^Arch" && pluglst+=(archlinux)
+    (( $+commands[dpkg] )) && pluglst+=(debian)
+    (( $+commands[dnf] )) && pluglst+=(dnf)
+
+    (( $+commands[yarn] )) && pluglst+=(yarn)
+    pluglst+=(nvm)
+
+    (( $+commands[rsync] )) && pluglst+=(rsync)
+    (( $+commands[git] )) && pluglst+=(git gitignore)
+    (( $+commands[systemctl] )) && pluglst+=(systemd)
+
+    if (( $+commands[xclip] || $+commands[wl-copy] )); then
+        pluglst+=(copybuffer copyfile copypath)
+    fi
+
+    pluglst+=(
+        extract z dirhistory shell-proxy
+        command-not-found
+        aliases alias-finder
+        sudo history
+    )
+
+    local omz_hint=(adb fd pip yarn docker rust)
+
+    loadomzplug() {
+        source "$omzdir/plugins/$1/$1.plugin.zsh"
+    }
+
+    source "$omzdir/lib/functions.zsh"
+    source "$omzdir/lib/completion.zsh"
+    source "$omzdir/lib/key-bindings.zsh"
+    source "$omzdir/lib/clipboard.zsh"
+
+    for plugname in $pluglst; do loadomzplug $plugname; done
+    unalias acs 2>/dev/null
+
+    for hintfile in $omz_hint; do fpath=("$omzdir/plugins/$hintfile" $fpath); done
+    autoload -U compinit
+    compinit
+
+    # others: common-aliases vagrant
+    # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/
+
+}
+
+__dmd_load_omz
--- modules/zsh/snippets/plugins.zsh
+++ modules/zsh/snippets/plugins.zsh
@@ -1,77 +1,25 @@
-__load_my_plugins() {
-	local omz_plugins=()
+__dmd_load_zplug() {
+	local zplugdir="$HOME/.deploy-my-dotfiles/zsh-plugins"
+	[[ ! -d "$zplugdir" ]] && return
 
-    lsb_release -si | grep -qi "^Arch" && omz_plugins+=(archlinux)
-	(( $+commands[dpkg] )) && omz_plugins+=(debian)
-	(( $+commands[dnf] )) && omz_plugins+=(dnf)
-
-	(( $+commands[yarn] )) && omz_plugins+=(yarn)
-	omz_plugins+=(nvm)
-
-	(( $+commands[rsync] )) && omz_plugins+=(rsync)
-	(( $+commands[git] )) && omz_plugins+=(git gitignore)
-	(( $+commands[systemctl] )) && omz_plugins+=(systemd)
-
-	if (( $+commands[xclip] || $+commands[wl-copy] )); then
-		omz_plugins+=(copybuffer copyfile copypath)
-	fi
-
-	omz_plugins+=(
-		extract z dirhistory shell-proxy
-		command-not-found
-		aliases alias-finder
-		sudo history
-	)
-
-	# other: zsh-interactive-cd
-	local omz_hint=(adb fd pip yarn docker rust)
-
-	local zplugins_dir="$HOME/.deploy-my-dotfiles/zsh-plugins"
-	local autosuggestions="$zplugins_dir/zsh-autosuggestions"
-	local syntax_highlighting="$zplugins_dir/zsh-syntax-highlighting"
-	omz="$zplugins_dir/ohmyzsh"
-
-	[ ! -d "$zplugins_dir" ] && return
-
+	local autosuggestions="$zplugdir/zsh-autosuggestions"
 	if [ -d "$autosuggestions" ]; then
 		source "$autosuggestions/zsh-autosuggestions.zsh"
 	fi
 
+	local syntax_highlighting="$zplugdir/zsh-syntax-highlighting"
 	if [ -d "$syntax_highlighting" ]; then
 		source "$syntax_highlighting/zsh-syntax-highlighting.zsh"
 	fi
 
-	if [ -d "$zplugins_dir/zsh-completions" ]; then
-		fpath=("$zplugins_dir/zsh-completions" $fpath)
+	if [ -d "$zplugdir/zsh-completions" ]; then
+		fpath=("$zplugdir/zsh-completions" $fpath)
 
 		rebuild_zcompdump() {
 			rm -f ~/.zcompdump
 			compinit
 		}
 	fi
-
-	if [ -d "$omz" ]; then
-	  __load_omzplug__dir="$omz"
-		loadomzplug() {
-			source "$__load_omzplug__dir/plugins/$1/$1.plugin.zsh"
-		}
-
-		source "$omz/lib/functions.zsh"
-		source "$omz/lib/completion.zsh"
-		source "$omz/lib/key-bindings.zsh"
-		source "$omz/lib/clipboard.zsh"
-
-		for plugin in $omz_plugins; do loadomzplug $plugin; done
-		for hint_plug in $omz_hint; do fpath=("$__load_omzplug__dir/plugins/$hint_plug" $fpath); done
-
-		autoload -U compinit
-		compinit
-
-		# others: common-aliases git rsync vagrant
-		# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/
-	fi
 }
 
-__load_my_plugins
-
-unalias acs 2> /dev/null
+__dmd_load_zplug
